8ebded32afeeef353e0070e74f88427e68e68daf
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2024-04-26 00:28:26 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -308,7 +308,7 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
         slit = lcaption.split("|")
         slit[0] = re_sub(r'\{([^}]+)\}', lowerVars, slit[0])
         up_path = ospath.join(dirpath, prefile_)
-        prefile_ = re_sub(r'www\S+', '', prefile_).replace('- ', '', 1)
+        prefile_ = re_sub(r'www\S+', '', prefile_).replace('- ', '', 1).replace('Tamil', '#Tamil').replace('Tam', '#Tam').replace('Malayalam', '#Malayalam').replace('Mal', '#Mal').replace('Telugu', '#Telugu').replace('Tel', '#Tel').replace('Hindi', '#Hindi').replace('Hin', '#Hin').replace('Kannada', '#Kannada').replace('Kan', '#Kan')
         dur, qual, lang, subs = await get_media_info(up_path, True)
         cap_mono = slit[0].format(
             filename = nfile_,
